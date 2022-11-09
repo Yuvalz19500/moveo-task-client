368a03e89f0a8e3ab788354f6c86a1ad0599c1fe
Yuval <yuvalz19500@gmail.com>
2022-11-10 01:42:42 +0200

    prod

--- src/app/endpoints/endpoints.ts
+++ src/app/endpoints/endpoints.ts
@@ -1,8 +1,8 @@
 import { environment } from "src/environments/environment";
 import { SweetPath } from "sweet-path";
 
-export const loginEndpoint = new SweetPath(environment.apiUrl + '/auth/login');
-export const getStudentsEndpoint = new SweetPath(environment.apiUrl + '/users/students');
-export const getCodeBlocksEndpoint = new SweetPath(environment.apiUrl + '/code-blocks');
-export const generateSessionLinkEndpoint = new SweetPath(environment.apiUrl + '/sessions')
-export const getCodeEndpoint = new SweetPath<'codeBlockId'>(environment.apiUrl + '/code-blocks/:codeBlockId')
+export const loginEndpoint = new SweetPath(environment.apiUrl + '/auth/login/');
+export const getStudentsEndpoint = new SweetPath(environment.apiUrl + '/users/students/');
+export const getCodeBlocksEndpoint = new SweetPath(environment.apiUrl + '/code-blocks/');
+export const generateSessionLinkEndpoint = new SweetPath(environment.apiUrl + '/sessions/')
+export const getCodeEndpoint = new SweetPath<'codeBlockId'>(environment.apiUrl + '/code-blocks/:codeBlockId/')
